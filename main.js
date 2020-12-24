cfbff7bf6e5fbf6ed2544edea9380346f678145f
meltaylor78 <meltaylor78@gmail.com>
2020-12-24 14:11:57 +0000

    module complete, code calling the functions. next, on to using the data

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
-Bytes
-var xhr = new XMLHttpRequest();
-
-xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
-xhr.send();
-
-xhr.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("data").innerHTML = this.responseText;
-    }
-};
+function getData(cb){
+    var xhr = new XMLHttpRequest();
+    
+    xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
+    xhr.send();
+    
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            cb(JSON.parse(this.responseText));
+        };
+    }; 
+}
+getData(function(data){
+    console.log(data)
+});
