e2a6824216bcae3c17b98571b74aaf0b7eabb6a3
meltaylor78 <meltaylor78@gmail.com>
2020-12-28 15:46:23 +0000

    Updating git after completing lesson

--- main.js
+++ main.js
@@ -1,15 +1,25 @@
-function getData(cb){
+const baseURL = "https://ci-swapi.herokuapp.com/api/";
+
+function getData(type, cb) {
     var xhr = new XMLHttpRequest();
-    
-    xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
-    xhr.send();
-    
+
     xhr.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             cb(JSON.parse(this.responseText));
-        };
-    }; 
+        }
+    };
+
+    xhr.open("GET", baseURL + type + "/");
+    xhr.send();
 }
-getData(function(data){
-    console.log(data)
-});
+
+function writeToDocument(type) {
+    var el = document.getElementById("data")
+    el.innerHTML = "";
+    getData(type, function(data) {
+        data = data.results;
+        data.forEach(function(item){
+            el.innerHTML += "<p>" + item.name + "</p>";
+        })
+    });
+}
